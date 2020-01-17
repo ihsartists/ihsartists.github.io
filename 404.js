9d1de6be9ce8da02c23c3e0d5888a07d5d76079b
Josh Chang <joshchang04@gmail.com>
2020-01-16 20:28:13 -0500

    Edit 404 Styles

---
+++ 404.js
@@ -0,0 +1,17 @@
+var deviceType = 'desktop';
+if(window.innerWidth < 1050){
+    $('#title-container').css('margin-top', '8px');
+}
+if(window.innerWidth < 900){
+    $('#title-container').css('margin-top', '3px');
+}
+if(window.innerWidth < 700){
+    deviceType = 'mobile';
+
+    $('#title-container').css('margin-left', '4%').css('margin-top', '0');
+    
+    $('#home-button').css('margin-right', '4%').css('margin-left', '4%');
+    $('#home-button-container').css('position', 'absolute').css('right', 'calc(2% + 4px)').css('top', '25px').css('margin', '0');
+    
+    $('#search-failed').css('margin-left', '4%').css('margin-top', '20px');
+}
